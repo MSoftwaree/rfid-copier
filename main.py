b2187f30eb11b86f4748c72ffb9308a55a65e3c8
Mariusz Strychowski <99507825+MSoftwaree@users.noreply.github.com>
2022-11-09 07:46:43 +0100

    Prepare main file

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("hello world")
+from rpi_lcd import LCD
+from core.io import IO
+from core.rfid import RFID
 
+
+lcd = LCD()
+io = IO()
+rfid = RFID(lcd)
+
+lcd.text("Welcome!", 1)
+
+while True:
+    lcd.text("Read or write?", 2)
+
+    io_state = io.read_buttons()
+
+    if io_state == "read":
+        rfid.read_card()
+    elif io_state == "write":
+        rfid.write_card()
+        
