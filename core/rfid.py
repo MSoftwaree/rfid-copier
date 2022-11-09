04ae97b6dd901cedf7e01c5c002d254ef7f250e4
Mariusz Strychowski <99507825+MSoftwaree@users.noreply.github.com>
2022-11-09 07:46:00 +0100

    Clean rfid code

--- core/rfid.py
+++ core/rfid.py
@@ -1,41 +1,36 @@
-import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from rpi_lcd import LCD
+import RPi.GPIO as GPIO
 import time
 
 
 class RFID:
-	def __init__(self):
+	def __init__(self, lcd):
 		self.reader = SimpleMFRC522()
-		self.lcd = LCD()
+		self.lcd = lcd
+		self.data = None
 
 	def read_card(self):
 		try:
 			self.lcd.text("Reading...", 1)
-			id, text = self.reader.read()
-			print(id)
-			print(text)
+			_, self.data = self.reader.read()
+			self.lcd.text("Read", 1)
 		finally:
 			time.sleep(2)
 			GPIO.cleanup()
 			self.lcd.clear()
 
 	def write_card(self):
+		if self.data is None:
+			self.lcd.text("No data read!", 1)
+			time.sleep(2)
+			return False
 		try:
-			text = input("New data: ")
-			self.lcd.text("Please hold the card", 1)
+			self.lcd.text("Hold the card", 1)
 			self.lcd.text("Writing...", 2)
-			self.reader.write(text)
-			self.lcd.text("Written", 2)
+			self.reader.write(self.data)
+			self.lcd.clear()
+			self.lcd.text("Written", 1)
 		finally:
 			time.sleep(2)
 			GPIO.cleanup()
 			self.lcd.clear()
-
-
-rfid = RFID()
-
-#rfid.write_card()
-rfid.read_card()
-
-print("Done")
